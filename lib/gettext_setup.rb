0f8db3666c24ec8132b937ee566f2fb23cc5e329
György Peng <gypeng@drezina.hu>
2013-08-18 14:55:10 +0200

    Update gettext_setup.rb
    local po commented out

--- lib/gettext_setup.rb
+++ lib/gettext_setup.rb
@@ -2,7 +2,7 @@
 # and at startup in production - to plug in theme locale strings.
 Rails.configuration.to_prepare do
     repos = [
-        FastGettext::TranslationRepository.build('app', :path=>File.join(File.dirname(__FILE__), '..', 'locale-theme'), :type => :po),
+#        FastGettext::TranslationRepository.build('app', :path=>File.join(File.dirname(__FILE__), '..', 'locale-theme'), :type => :po),
         FastGettext::TranslationRepository.build('app', :path=>'locale', :type => :po)
     ]
     FastGettext.add_text_domain 'app', :type=>:chain, :chain=>repos
